8f9d2608deacd2edd2b6924a65bd211e978c4c55
Myron Turner <turnermm02@shaw.ca>
2011-05-29 11:45:29 -0500

    Correection to authorship attribution in action.php

--- action.php
+++ action.php
@@ -5,7 +5,7 @@ require_once(DOKU_PLUGIN.'action.php');
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Pierre Spring <pierre.spring@liip.ch>
+ * @author     Myron Turner <turnermm02@shaw.ca>
  */
 
 class action_plugin_openas extends DokuWiki_Action_Plugin {
