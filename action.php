3bc0fc874f0febaa79e5843fcad411cb30c7068a
Myron Turner <turnermm02@shaw.ca>
2011-05-29 11:36:25 -0500

    Initialized OpenAs plugin for Dokuwiki

---
+++ action.php
@@ -0,0 +1,51 @@
+<?php
+if(!defined('DOKU_INC')) define('DOKU_INC',realpath(dirname(__FILE__).'/../../../').'/');
+if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
+require_once(DOKU_PLUGIN.'action.php');
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Pierre Spring <pierre.spring@liip.ch>
+ */
+
+class action_plugin_openas extends DokuWiki_Action_Plugin {
+    //store the namespaces for sorting
+    var $sortEdit = array();
+    var $helper = false;
+    var $commit = false;
+
+    /**
+     * Constructor
+     */
+    function action_plugin_fckg_delete(){
+    }
+
+    function getInfo() {
+        return array(
+            'author' => 'Pierre Spring',
+            'email'  => 'pierre.spring@liip.ch',
+            'date'   => '2007-Mai-08',
+            'name'   => 'fckg_delete',
+            'desc'   => 'Delete Plugin for the Dokuwiki FCKW Plugin',
+            'url'    => 'https://fosswiki.liip.ch/display/FCKW/Home');
+    }
+
+    function register(&$controller) {
+        $controller->register_hook('DOKUWIKI_STARTED', 'BEFORE', $this, 'openas_preprocess');
+    }
+
+    function openas_preprocess(&$event){
+
+        if(isset($_REQUEST['openas']) && $_REQUEST['openas'] == 'delete') {
+              $id = wikiFN($_REQUEST['id']); 
+              if(file_exists($id)) {
+                  $file = wikiFN($_REQUEST['saveas_orig']); 
+                  if(file_exists($file)) @unlink($file);
+              }
+        }
+
+    }
+
+
+} //end of action class
+?>
---
+++ syntax.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Plugin OpenAs.
+ * 
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author    Myron Turner <turnermm02@shaw.ca>
+ */
+
+// must be run within DokuWiki
+if(!defined('DOKU_INC')) die();
+//ini_set("display_errors","1");
+if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
+require_once DOKU_PLUGIN.'syntax.php';
+
+/**
+ * All DokuWiki plugins to extend the parser/rendering mechanism
+ * need to inherit from this class
+ */
+class syntax_plugin_openas extends DokuWiki_Syntax_Plugin {
+
+    function getInfo() {
+        return array('author' => 'Myron Turner',
+                     'email'  => 'turnermm02@shaw.ca',
+                     'date'   => '2011-05-29',
+                     'name'   => 'OpenAs Plugin',
+                     'desc'   => 'File utility for moving and saving-as, and creating new pages from templates',
+                     'url'    => 'http://www.dokuwiki.org/plugin:openas');
+    }
+
+    function getType() { return 'substition'; }
+    function getSort() { return 80; }
+    function getPType() { return 'block'; }
+    function connectTo($mode) {
+        $this->Lexer->addSpecialPattern('~~SaveAS>.*?~~',$mode,'plugin_openas');
+        $this->Lexer->addSpecialPattern('~~OpenAS>.*?~~',$mode,'plugin_openas');
+        $this->Lexer->addSpecialPattern('~~MoveTO>.*?~~',$mode,'plugin_openas');
+    }
+
+    function handle($match, $state, $pos, &$handler) {
+         global $ID;
+         $actions = array('SaveAS' => 'save', 'MoveTO' => 'delete');
+         $file = wikiFN($ID);
+         list($type,$name,$newpagevars) = explode('>',(trim($match,'~')));
+           
+               
+         if($type == 'SaveAS' || $type == 'MoveTO') {
+           $action = $actions[$type];
+           $newfile = wikiFN($name);           
+           $contents = file_get_contents($file);
+           $contents = preg_replace('/~~' . $type .'.*?~~/',"",$contents,1);
+           io_saveFile($newfile,$contents);   
+           $wikilink = html_wikilink("$name?saveas_orig=$ID&openas=$action");     
+           $match = "Click on this link to open your renamed page:<br /> $wikilink";  
+         }
+         else if($type == 'OpenAS') {
+           list($id,$template) = explode('#',$name);
+           $newpagevars = urlencode($newpagevars); 
+           $match = html_wikilink("$id?do=edit&rev=&newpagetemplate=:pagetemplates:$template&newpagevars=$newpagevars");     
+         }
+         
+         return array($state,$match);
+    }
+
+    function render($mode, &$renderer, $data) {
+        list($state,$match) = $data;
+       
+        if($mode == 'xhtml'){
+            $renderer->doc .= '<div class="save_as">' . $match . '</div>';
+            return true;
+        }
+        return false;
+    }
+
+  function write_debug($what) {   
+     $handle = fopen("saveas.txt", "a");
+     if(is_array($what)) $what = print_r($what,true);
+     fwrite($handle,"$what\n");
+     fclose($handle);
+  }
+}
