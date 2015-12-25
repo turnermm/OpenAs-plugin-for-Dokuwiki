d340fb5e00853219dd4f69ce6739a9a6d775a717
Myron Turner <turnermm02@shaw.ca>
2015-12-25 12:30:31 -0600

    brought <action>::register declaration into conformity with PHP 7.0

--- action.php
+++ action.php
@@ -31,7 +31,7 @@ class action_plugin_openas extends DokuWiki_Action_Plugin {
             'url'    => 'http://www.dokuwiki.org/plugin:openas');
     }
 
-    function register(&$controller) {
+    function register(Doku_Event_Handler $controller) {
         $controller->register_hook('DOKUWIKI_STARTED', 'BEFORE', $this, 'openas_preprocess');
 		$controller->register_hook('DOKUWIKI_DONE', 'BEFORE', $this, 'update_locked_pages');
     }
