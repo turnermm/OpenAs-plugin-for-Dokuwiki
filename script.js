3b5d088b1dcc47df5ae3524317551e7523eca09c
Myron Turner <turnermm02@shaw.ca>
2016-11-27 15:40:40 -0600

    error check for empty page id

--- script.js
+++ script.js
@@ -5,7 +5,13 @@ jQuery(document).ready(function(){
        var jq = jQuery(this);
        var href = jq.attr('href');    
       var id = jQuery("#save_as_info input[name=save_as_page]").val() ;
+      if(!id)   {
+          alert("You have left the page id blank.")
+          return false;
+      }  
+         else {
       href = href.replace(/SAVEAS_PAGE/i, id);
       jq.attr("href",href);  //   (jQuery("div.save_as_tmpl a.wikilink2").attr("href",href));
+     }
     });
 });
